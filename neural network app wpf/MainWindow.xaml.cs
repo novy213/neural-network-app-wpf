a7928404ac9d6a850b155facda198e1aa0b1827e
Jakub Solarek <j.solarek@wp.pl>
2022-09-02 19:26:26 +0200

    Add insert values grid and items count grid

--- neural network app wpf/MainWindow.xaml.cs
+++ neural network app wpf/MainWindow.xaml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -28,7 +29,48 @@ namespace neural_network_app_wpf
         private void StartButton_click(object sender, RoutedEventArgs e)
         {
             MenuGrid.Visibility = Visibility.Collapsed;
-            MainGrid.Visibility = Visibility.Visible;
+            ItemsCountGrid.Visibility = Visibility.Visible;
+        }
+
+        private void NextNumberFiles_click(object sender, RoutedEventArgs e)
+        {
+            if (numberOfItemsXAML.Text.Length > 0)
+            {
+                ItemsCountGrid.Visibility = Visibility.Collapsed;
+                InsertValues.Visibility = Visibility.Visible;
+            }
+            else
+            {
+                Alert.Text = "This field cannot be empty";
+            }
+        }
+
+        private void CancelNumberFiels_click(object sender, RoutedEventArgs e)
+        {
+            Alert.Text = null;
+            MenuGrid.Visibility = Visibility.Visible;
+            ItemsCountGrid.Visibility = Visibility.Collapsed;
+        }
+        private static readonly Regex _regex = new Regex("[^0-9.-]+"); 
+        private static bool IsTextAllowed(string text)
+        {
+            return !_regex.IsMatch(text);
+        }
+
+        private void numberOfItemsXAML_PreviewTextInput(object sender, TextCompositionEventArgs e)
+        {
+            e.Handled = !IsTextAllowed(e.Text);
+        }
+
+        private void InsertValueBack_click(object sender, RoutedEventArgs e)
+        {
+            ItemsCountGrid.Visibility = Visibility.Visible;
+            InsertValues.Visibility = Visibility.Collapsed;
+        }
+
+        private void InsertValueNext_click(object sender, RoutedEventArgs e)
+        {
+
         }
     }
 }
