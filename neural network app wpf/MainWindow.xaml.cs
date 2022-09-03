466f46f7b86601ee6003f2397fc48457903c98d9
Jakub Solarek <j.solarek@wp.pl>
2022-09-03 18:18:29 +0200

    Add main grid funciotnality for one attemp

--- neural network app wpf/MainWindow.xaml.cs
+++ neural network app wpf/MainWindow.xaml.cs
@@ -29,9 +29,20 @@ namespace neural_network_app_wpf
         int[] diameter = new int[6];
         int[] length = new int[6];
 
+        Random r =new Random();
+
+        double[] weights = new double[2];
+        double[] oldWeights = new double[2];
+        double mutaion;
+        int idWrongCounter = 0;
+        int attempCounter = 2;
+
         public MainWindow()
         {
             InitializeComponent();
+            mutaion = r.Next(-1000, 1000);
+            weights[0] = r.Next(-1000, 1000);
+            weights[1] = r.Next(-1000, 1000);
         }
 
         private void StartButton_click(object sender, RoutedEventArgs e)
@@ -82,7 +93,54 @@ namespace neural_network_app_wpf
 
         private void InsertValueNext_click(object sender, RoutedEventArgs e)
         {
-
+            if (currentValue == numberOfItems)
+            {
+                InsertValues.Visibility = Visibility.Collapsed;
+                MainGrid.Visibility = Visibility.Visible;
+                for (int i = 0; i < numberOfItems; i++)
+                {
+                    result[i] = ((diameter[i] * weights[0]) + (length[i] * weights[1])) + mutaion;
+                    if (result[i] > 0)
+                    {
+                        computerThingName[i] = "Ring";
+                    }
+                    else if (result[i] < 0)
+                    {
+                        computerThingName[i] = "Pen";
+                    }
+                    if (result[i] > 0 && correctAnswer[i] == 1)
+                    {
+                        correct[i] = true;
+                        /*Console.Write(result[i] + " " + "Computer {0}, we {1}", computerThingName[i], thingName[i] + " ");
+                        Console.ForegroundColor = ConsoleColor.Green;
+                        Console.WriteLine(correct[i]);
+                        Console.ForegroundColor = ConsoleColor.White;*/
+                        this.ResultListView.Items.Add(new MyItem { Computer = computerThingName[i], We = thingName[i], Compare = correct[i] });
+                    }
+                    else if (result[i] < 0 && correctAnswer[i] == 2)
+                    {
+                        correct[i] = true;
+                        /*Console.Write(result[i] + " " + "Computer {0}, we {1}", computerThingName[i], thingName[i] + " ");
+                        Console.ForegroundColor = ConsoleColor.Green;
+                        Console.WriteLine(correct[i]);
+                        Console.ForegroundColor = ConsoleColor.White;*/
+                        this.ResultListView.Items.Add(new MyItem { Computer = computerThingName[i], We = thingName[i], Compare = correct[i] });
+                    }
+                    else
+                    {
+                        correct[i] = false;
+                        /*Console.Write(result[i] + " " + "Computer {0}, we {1}", computerThingName[i], thingName[i] + " ");
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        Console.WriteLine(correct[i]);
+                        Console.ForegroundColor = ConsoleColor.White;*/
+                        this.ResultListView.Items.Add(new MyItem { Computer = computerThingName[i], We = thingName[i], Compare = correct[i] });
+                    }
+                }
+            }
+            else
+            {
+                Alert1.Text = "No value was given";
+            }
         }
 
         private void InsertValueAdd(object sender, RoutedEventArgs e)
@@ -127,6 +185,19 @@ namespace neural_network_app_wpf
             {
                 Alert1.Text = "No more value can be added";
             }
+        }
+        public class MyItem
+        {
+            public string Computer { get; set; }
+
+            public string We { get; set; }
+            public bool Compare { get; set; }
+        }
+
+        private void MainGridStop_click(object sender, RoutedEventArgs e)
+        {
+            MainGrid.Visibility = Visibility.Collapsed;
+            MenuGrid.Visibility = Visibility.Visible;
         }
     }
 }
